9bad0cb3461b7cf884c580ca784241cbef403f5c
Roneet Kumar <roneetparida@gmail.com>
2025-02-04 19:11:06 -0500

    clean up

---
+++ apps/interface/src/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Noto_Sans } from "next/font/google";
+import "@styles";
+
+
+
+const notoSans = Noto_Sans({
+  variable: "--font-noto-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Interface",
+  description: "Interface",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${notoSans.variable} ${notoSans.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ apps/interface/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="pt-12 text-center text-6xl">Hello Interface</h1>
+    </div>
+  );
+}
