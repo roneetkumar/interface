24711c1649bcf645fc61c953e454f63d064be8b1
Sumit Parida <80693203+sumitparida2002@users.noreply.github.com>
2025-02-06 17:05:51 -0500

    removed radix ui themes just primitive for now

--- apps/interface/src/app/layout.tsx
+++ apps/interface/src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Noto_Sans } from "next/font/google";
 import "@styles";
-import "@radix-ui/themes/styles.css";
-import { Theme } from "@radix-ui/themes";
 
 const notoSans = Noto_Sans({
   variable: "--font-noto-sans",
@@ -21,13 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Theme>
-        <body
-          className={`${notoSans.variable} ${notoSans.variable} antialiased`}
-        >
-          {children}
-        </body>
-      </Theme>
+      <body className={`${notoSans.variable} ${notoSans.variable} antialiased`}>
+        {children}
+      </body>
     </html>
   );
 }
